56c60e8e2e1d81c845d18b39a2692048f3acb95e
Alexia Wilson <alexia.wilson@mediant.in>
2014-04-08 01:11:29 -0700

    GUI skeleton created

---
+++ Friendly folder icon customization/ResourcePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Friendly_folder_icon_customization
+{
+    /// <summary>
+    /// Interaction logic for ResourcePage.xaml
+    /// </summary>
+    public partial class ResourcePage : Page
+    {
+        public ResourcePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
